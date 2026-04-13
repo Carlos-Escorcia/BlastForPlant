f2c2cf73139162e1479afea462e22470dbcf8b7c
Tiuri96 <al449001@uji.es>
2026-04-13 10:35:56 +0200

    Creación de niveles

--- Assets/Scripts/CambioEscena.cs
+++ Assets/Scripts/CambioEscena.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; //Cambio de escenas
 
 public class CambioEscena : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Configuración del Nivel")]
+    [Tooltip("Escribe aquí el nombre EXACTO de la escena a la que quieres ir")]
+    public string nombreSiguienteEscena;
 
-    // Update is called once per frame
-    void Update()
+    //Esta función se activa automáticamente cuando alguien entra en la zona "Trigger"
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        //Comprobamos si el objeto que acaba de entrar tiene la etiqueta "Player" (tu personaje)
+        if (collision.CompareTag("Player"))
+        {
+            //Si es el jugador, cargamos la escena que hayas escrito en el Inspector
+            SceneManager.LoadScene(nombreSiguienteEscena);
+        }
     }
 }
