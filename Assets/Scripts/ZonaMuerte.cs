d3a6e19445dd5d3b1ae8340e30e5e098778735db
Tiuri96 <al449001@uji.es>
2026-04-13 23:11:21 +0200

    Punto de respawn
    Se ha creado el punto de respawn, así como un detector de caídas y se ha puesto más tiles de suelo

---
+++ Assets/Scripts/ZonaMuerte.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZonaMuerte : MonoBehaviour
+{
+    public Transform puntoDeRespawn;
+    public string escenaGameOver = "GameOver";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ControlPersonaje personaje = collision.GetComponent<ControlPersonaje>();
+
+            if (personaje != null)
+            {
+                // !!! LLAMAMOS A LA NUEVA FUNCIÓN DEL PERSONAJE !!!
+                personaje.PerderVida();
+
+                if (personaje.vidas > 0)
+                {
+                    collision.transform.position = puntoDeRespawn.position;
+                }
+                else
+                {
+                    SceneManager.LoadScene(escenaGameOver);
+                }
+            }
+        }
+    }
+}
