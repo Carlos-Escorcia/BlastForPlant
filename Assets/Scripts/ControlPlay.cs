4656f7a82d774965c186793635a3a5902b5b179e
Tiuri96 <al449001@uji.es>
2026-04-18 13:13:45 +0200

    Código botón play

---
+++ Assets/Scripts/ControlPlay.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ControlPlay : MonoBehaviour
+{
+    public Animator animadorBoton;
+    public float tiempoDeEspera = 0.5f;
+
+    public void PulsarPlay()
+    {
+        StartCoroutine(RutinaEsperaYCambio());
+    }
+
+    private IEnumerator RutinaEsperaYCambio()
+    {
+        //Activa la animación pulsar del botón
+        if (animadorBoton != null) animadorBoton.SetTrigger("Pulsar");
+        //Espera medio segundo
+        yield return new WaitForSeconds(tiempoDeEspera);
+        SceneManager.LoadScene("Nivel 1");
+    }
+}
